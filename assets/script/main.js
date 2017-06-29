4bddd0e664065f8ce6b90a889ec5deb5c0ad5ca7
Lunecia <lunecia@Lunecia-MBP.local>
2017-06-29 20:56:33 +0700

    title update

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,4 +4,21 @@
 $('.hamberger').on('click', function () {
 	$(this).toggleClass('open');
 	$('.nav-bar').toggleClass('open');
+	$('.wrapper').toggleClass('blur');
+});
+$('.nav-bar').find('li').on('click', function () {
+	$('.hamberger').toggleClass('open');
+	$('.nav-bar').toggleClass('open');
+	$('.wrapper').toggleClass('blur');
+});
+
+// preload
+$(window).on("load", function () {
+	setTimeout(function () {
+		$('.loading').fadeOut();
+	}, 1500);
+	setTimeout(function () {
+		$('header').fadeIn();
+		$('.scroll').fadeIn();
+	}, 5500);
 });

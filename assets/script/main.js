fee292dde55b49b1621870ea6e25da6f7e49b4bf
Nuttapon <nuttapon.phannurat@isobar.com>
2017-06-29 20:45:39 +0700

    Update About Us

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,30 @@
-"use strict";
+'use strict';
+
+$(function () {
+	$('#circle1').hover(function () {
+		$('#circle1').addClass('active');
+		$('#circle2').removeClass('active');
+		$('#circle3').removeClass('active');
+		$('#service-desc1').fadeIn();
+		$('#service-desc2').css('display', 'none');
+		$('#service-desc3').css('display', 'none');
+	}, function () {});
+
+	$('#circle2').hover(function () {
+		$('#circle2').addClass('active');
+		$('#circle1').removeClass('active');
+		$('#circle3').removeClass('active');
+		$('#service-desc2').fadeIn();
+		$('#service-desc1').css('display', 'none');
+		$('#service-desc3').css('display', 'none');
+	}, function () {});
+
+	$('#circle3').hover(function () {
+		$('#circle3').addClass('active');
+		$('#circle1').removeClass('active');
+		$('#circle2').removeClass('active');
+		$('#service-desc3').fadeIn();
+		$('#service-desc1').css('display', 'none');
+		$('#service-desc2').css('display', 'none');
+	}, function () {});
+});
---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+$(() => {
+	$('#circle1').hover(() =>{
+		$('#circle1').addClass('active');
+		$('#circle2').removeClass('active');
+		$('#circle3').removeClass('active');
+		$('#service-desc1').fadeIn();
+		$('#service-desc2').css('display', 'none');
+		$('#service-desc3').css('display', 'none');
+	},
+	() => {
+
+	});
+
+	$('#circle2').hover(() =>{
+		$('#circle2').addClass('active');
+		$('#circle1').removeClass('active');
+		$('#circle3').removeClass('active');
+		$('#service-desc2').fadeIn();
+		$('#service-desc1').css('display', 'none');
+		$('#service-desc3').css('display', 'none');
+	},
+	() => {
+
+	});
+
+	$('#circle3').hover(() =>{
+		$('#circle3').addClass('active');
+		$('#circle1').removeClass('active');
+		$('#circle2').removeClass('active');
+		$('#service-desc3').fadeIn();
+		$('#service-desc1').css('display', 'none');
+		$('#service-desc2').css('display', 'none');
+	},
+	() => {
+		
+	});
+});

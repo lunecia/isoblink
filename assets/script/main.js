8227664dd0d5d6839745cd1a42301ccd77ac3768
lunecia <happie.lu@gmail.com>
2017-08-02 17:10:36 +0700

    title update

--- assets/script/main.js
+++ assets/script/main.js
@@ -59,10 +59,10 @@ $(window).on("load", function () {
 	setTimeout(function () {
 		$('header').fadeIn();
 		$('.scroll').fadeIn();
-	}, 5500);
+	}, 2500);
 	setTimeout(function () {
 		$('.enter-site').addClass('loaded');
-	}, 6000);
+	}, 2500);
 });
 
 // scrollTo
@@ -88,4 +88,194 @@ $('.tilt-box2').tilt({
 	scale: 1,
 	maxGlare: 0.3,
 	perspective: 1500
+});
+
+// title
+
+
+var mouseX = 0,
+    mouseY = 0,
+    windowHalfX = window.innerWidth / 2,
+    windowHalfY = window.innerHeight / 2,
+    SEPARATION = 200,
+    AMOUNTX = 10,
+    AMOUNTY = 10,
+    camera,
+    scene,
+    renderer;
+
+init();
+animate();
+
+function init() {
+
+	var container,
+	    separation = 100,
+	    amountX = 50,
+	    amountY = 50,
+	    particle,
+	    title = document.getElementById('backgroundAni');;
+
+	container = document.createElement('div');
+	backgroundAni.appendChild(container);
+
+	scene = new THREE.Scene();
+
+	renderer = new THREE.CanvasRenderer({ alpha: true }); // gradient; this can be swapped for WebGLRenderer
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	container.appendChild(renderer.domElement);
+
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+	camera.position.z = 100;
+
+	// particles
+	var PI2 = Math.PI * 2;
+	var material = new THREE.SpriteCanvasMaterial({
+		color: 0xffffff,
+		program: function program(context) {
+			context.beginPath();
+			context.arc(0, 0, 0.5, 0, PI2, true);
+			context.fill();
+		}
+	});
+
+	var geometry = new THREE.Geometry();
+
+	for (var i = 0; i < 100; i++) {
+		particle = new THREE.Sprite(material);
+		particle.position.x = Math.random() * 2 - 1;
+		particle.position.y = Math.random() * 2 - 1;
+		particle.position.z = Math.random() * 2 - 1;
+		particle.position.normalize();
+		particle.position.multiplyScalar(Math.random() * 10 + 450);
+		particle.scale.x = particle.scale.y = 10;
+		scene.add(particle);
+		geometry.vertices.push(particle.position);
+	}
+
+	// lines
+	var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0xffffff, opacity: 0.5 }));
+	scene.add(line);
+
+	// mousey
+	document.addEventListener('mousemove', onDocumentMouseMove, false);
+	document.addEventListener('touchstart', onDocumentTouchStart, false);
+	document.addEventListener('touchmove', onDocumentTouchMove, false);
+
+	window.addEventListener('resize', onWindowResize, false);
+} // end init();
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onDocumentMouseMove(event) {
+
+	mouseX = event.clientX - windowHalfX;
+	mouseY = event.clientY - windowHalfY;
+}
+
+function onDocumentTouchStart(event) {
+
+	if (event.touches.length > 1) {
+
+		event.preventDefault();
+
+		mouseX = event.touches[0].pageX - windowHalfX;
+		mouseY = event.touches[0].pageY - windowHalfY;
+	}
+}
+
+function onDocumentTouchMove(event) {
+
+	if (event.touches.length == 1) {
+
+		event.preventDefault();
+
+		mouseX = event.touches[0].pageX - windowHalfX;
+		mouseY = event.touches[0].pageY - windowHalfY;
+	}
+}
+
+function animate() {
+
+	requestAnimationFrame(animate);
+	render();
+}
+
+function render() {
+
+	camera.position.x += (mouseX - camera.position.x) * .05;
+	camera.position.y += (-mouseY + 200 - camera.position.y) * .05;
+	camera.lookAt(scene.position);
+
+	renderer.render(scene, camera);
+}
+
+// word
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+	splitLetters(words[i]);
+}
+
+function changeWord() {
+	var cw = wordArray[currentWord];
+	var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+	for (var i = 0; i < cw.length; i++) {
+		animateLetterOut(cw, i);
+	}
+
+	for (var i = 0; i < nw.length; i++) {
+		nw[i].className = 'letter behind';
+		nw[0].parentElement.style.opacity = 1;
+		animateLetterIn(nw, i);
+	}
+
+	currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+	setTimeout(function () {
+		cw[i].className = 'letter out';
+	}, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+	setTimeout(function () {
+		nw[i].className = 'letter in';
+	}, 340 + i * 80);
+}
+
+function splitLetters(word) {
+	var content = word.innerHTML;
+	word.innerHTML = '';
+	var letters = [];
+	for (var i = 0; i < content.length; i++) {
+		var letter = document.createElement('span');
+		letter.className = 'letter';
+		letter.innerHTML = content.charAt(i);
+		word.appendChild(letter);
+		letters.push(letter);
+	}
+
+	wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
+$('.text.main').hover(function () {
+	$(this).find('span').toggleClass('test');
+	// $(this).find('span').text('test');
 });
